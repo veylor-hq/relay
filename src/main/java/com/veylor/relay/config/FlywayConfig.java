7ddc771466913d9780f8e3b6c1a7a32de976d67c
Ihor Savenko <csigorek@gmail.com>
2026-07-18 00:55:34 +0100

    feat(flyway): Add Flyway Config

---
+++ src/main/java/com/veylor/relay/config/FlywayConfig.java
@@ -0,0 +1,27 @@
+package com.veylor.relay.config;
+
+import jakarta.annotation.PostConstruct;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    private final DataSource dataSource;
+
+    public FlywayConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @PostConstruct
+    public void migrate() {
+        Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load()
+                .migrate();
+    }
+}
